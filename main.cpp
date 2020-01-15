5fb57b7d81e23037d82e14379fec406dd4b259e2
Chris Hartman <chris.m.hartman@gmail.com>
2020-01-15 14:46:53 -0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+  cout << "Welcome to the grading AI for CS 372 Spring 2020.\n";
+  cout << "The following grades should be recorded:\n";
+  cout << "Professor Hartman gets an A because he enters the grades (and wrote this.)\n";
+  return 0;
+}

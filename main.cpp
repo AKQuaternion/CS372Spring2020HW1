b6d245fd62ba11384b8d906e2873fb7bc0bc1f95
TailonR <37911691+TailonR@users.noreply.github.com>
2020-01-16 14:53:16 -0900

    Adding cout message for Tailon R.

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main() {
   cout << "Welcome to the grading AI for CS 372 Spring 2020.\n";
   cout << "The following grades should be recorded:\n";
   cout << "Professor Hartman gets an A because he enters the grades (and wrote this.)\n";
+  cout << "Tailon Russell should get an A because he has a bunch of screenshots and is the first person to get this far.\n";
   return 0;
 }

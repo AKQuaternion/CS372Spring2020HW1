62e421a21dc155cd6c98b171c5a14b93e32455f0
Douglas Sorensen <douglassorensen@McKades-MBP.gci.net>
2020-01-18 17:26:07 -0900

    Added McKades grade

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
   cout << "Professor Hartman gets an A because he enters the grades (and wrote this.)\n";
   cout << "Tailon Russell should get an A because he has a bunch of screenshots and is the first person to get this far.\n";
   cout << "Zachary Mason should get an A because he was the second one to get this far... first is the worst - second is the best, Tailon ;)\n";
+  cout << "McKade Sorensen should get an A because according to the line above he has a hairy chest.\n";
   return 0;
 }

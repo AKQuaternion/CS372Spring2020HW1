bb31c478f3c48373880742cbb9652a72213db6e5
Nick <scifi6546@protonmail.com>
2020-01-22 13:11:58 -0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ int main() {
   cout << "Welcome to the grading AI for CS 372 Spring 2020.\n";
   cout << "The following grades should be recorded:\n";
   cout << "Professor Hartman gets an A because he enters the grades (and wrote this.)\n";
+
   cout << "Tailon Russell should get an A because he has a bunch of screenshots and is the first person to get this far.\n";
   cout << "Zachary Mason should get an A because he was the second one to get this far... first is the worst - second is the best, Tailon ;)\n";
   cout << "McKade Sorensen should get an A because according to the line above he has a hairy chest.\n";
   cout << "Theng Yang should get an A+ because none of the above asked for an A+.\n";
   cout << "Chenyi Ling should get an A because he has a super smart cat.\n";
   cout << "Blake Caldwell sould get an A becuase he is finally using Git effectivly. \n";
-  cout << "Matt Perry should get an A because no typos. \n";
+  std::cout<<"Nicholas Alexeev should definatly get an A+";
   return 0;
 }

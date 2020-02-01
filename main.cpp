e353924efcf0cb09b2f2de1d12405fb097fee4b6
Ben Talotta <bmtalotta@alaska.edu>
2020-02-01 12:42:10 -0900

    Update main.cpp
    updated main to add name for Ben Talotta

--- main.cpp
+++ main.cpp
@@ -23,6 +23,6 @@ int main() {
   cout << "Svein Ormseth should get an A because he's a cool guy\n";
   cout << "Shihhsien Wu Should get an A- because he might cause merge conflicts. \n";
   cout << "Greg Talotta should get an A because he is trying his best to learn git. \n";
-
+  cout << "Ben Talotta should get an A- because he did the assignment but turned it in a little late. \n";
   return 0;
 }
